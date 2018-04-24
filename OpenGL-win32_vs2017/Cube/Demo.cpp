d310b0273a16f7baef5048fe934bafe99f7d7ac1
DESKTOP-U5ORU59\Rickshao <rickshao@foxmail.com>
2018-04-24 23:54:11 +0800

    [Add Feature]开始3D绘制

---
+++ OpenGL-win32_vs2017/Cube/Demo.cpp
@@ -0,0 +1,84 @@
+﻿
+#include <windows.h>
+#include <tchar.h>
+#include <math.h>
+#include "CELLMath.hpp"
+#include<gl/glu.h>
+#include "OpenGLWindow.h"
+#include <vector>
+using namespace CELL;
+
+struct Vertex {
+	float x, y, z;
+};
+
+class   Demo :public OpenGLWindow
+{
+public:
+	Demo()
+	{
+	}
+
+	virtual void    render()
+	{
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+
+		gluPerspective(60, double(_width) / double(_height), 0.1, 1000);
+
+		Vertex cubeVertices[] = {
+			{ -1.0f,-1.0f,  1.0f },
+			{ 1.0f, -1.0f,  1.0f },
+			{ 1.0f, 1.0f,   1.0f },
+			{ -1.0f, 1.0f,  1.0f },
+
+			{ -1.0f,-1.0f,  -1.0f },
+			{ -1.0f, 1.0f,  -1.0f },
+			{ 1.0f, 1.0f,   -1.0f },
+			{ 1.0f,-1.0f,   -1.0f },
+
+			{ -1.0f, 1.0f,  -1.0f },
+			{ -1.0f, 1.0f,  1.0f },
+			{ 1.0f, 1.0f,   1.0f },
+			{ 1.0f, 1.0f,   -1.0f },
+
+			{ -1.0f,-1.0f,  -1.0f },
+			{ 1.0f,-1.0f,   -1.0f },
+			{ 1.0f,-1.0f,   1.0f },
+			{ -1.0f,-1.0f,  1.0f },
+
+			{ 1.0f,-1.0f,  -1.0f },
+			{ 1.0f, 1.0f,  -1.0f },
+			{ 1.0f, 1.0f,  1.0f },
+			{ 1.0f,-1.0f,  1.0f },
+
+			{ -1.0f,-1.0f,  -1.0f },
+			{ -1.0f,-1.0f,  1.0f },
+			{ -1.0f, 1.0f,  1.0f },
+			{ -1.0f, 1.0f,  -1.0f }
+		};
+		for (int i = 0; i < sizeof(cubeVertices) / sizeof(cubeVertices[0]); ++i)
+		{
+			cubeVertices[i].z -= 5;
+		}
+
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glVertexPointer(3, GL_FLOAT, sizeof(Vertex), cubeVertices);
+		glDrawArrays(GL_QUADS, 0, 24);
+	}
+};
+
+int __stdcall WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nShowCmd
+)
+{
+
+	Demo    instance;
+	instance.main(800, 600);
+
+
+	return  0;
+}
---
+++ OpenGL-win32_vs2017/Perspertive/Demo.cpp
@@ -0,0 +1,51 @@
+﻿
+#include <windows.h>
+#include <tchar.h>
+#include <math.h>
+#include "CELLMath.hpp"
+#include "OpenGLWindow.h"
+#include <gl/glu.h>
+#include <vector>
+using namespace CELL;
+
+class   Demo :public OpenGLWindow
+{
+public:
+	Demo()
+	{
+	}
+
+	virtual void    render()
+	{
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+
+		gluPerspective(60, double(_width) / double(_height), 0.1, 1000);
+
+		float3  arVert[] =
+		{
+			float3(0.5f,0,-0.5),
+			float3(0.2f,0.3,-.5),
+			float3(0.8f,0.3,-.5),
+		};
+
+		glEnableClientState(GL_VERTEX_ARRAY);
+		glVertexPointer(3, GL_FLOAT, sizeof(float3), arVert);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+	}
+};
+
+int __stdcall WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nShowCmd
+)
+{
+
+	Demo    instance;
+	instance.main(800, 600);
+
+
+	return  0;
+}
